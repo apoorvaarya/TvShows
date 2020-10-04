52313cc9684079722f6f12833f6b6a61a2dde65d
Apoorva <apoorvaarya>
2020-10-04 18:53:51 -0400

    commit

--- app/src/main/java/com/example/tvshows/TvData.java
+++ app/src/main/java/com/example/tvshows/TvData.java
@@ -1,4 +1,32 @@
 package com.example.tvshows;
 
 public class TvData {
+
+    private String itemName;
+    private String itemGenre;
+    private String itemRate;
+    private int itemImage;
+
+    public TvData(String itemName, String itemGenre, String itemRate, int itemImage) {
+        this.itemName = itemName;
+        this.itemGenre = itemGenre;
+        this.itemRate = itemRate;
+        this.itemImage = itemImage;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemGenre() {
+        return itemGenre;
+    }
+
+    public String getItemRate() {
+        return itemRate;
+    }
+
+    public int getItemImage() {
+        return itemImage;
+    }
 }
